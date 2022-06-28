2116e1bb8cef4a204beb0a7a898d9c9529538619
Yogesh Machhirke <63632729+YOGESHMACHHIRKE@users.noreply.github.com>
2022-06-28 08:24:22 +0530

    Add files via upload

---
+++ tut1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+//this is comment and will not run during execution runt he programm
+
+int main(){
+
+    int sum = 6;
+cout<<"Hello Yogesh"<< sum;
+return 0;
+
+}
